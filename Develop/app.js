7370f5f62f6ece9986bd484e159c6f8858f0189e
paolorafaelyap <paolorafaelyap@gmail.com>
2020-12-12 16:51:40 -0800

    inquirer code for gathering team member info

--- Develop/app.js
+++ Develop/app.js
@@ -9,11 +9,45 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { ADDRGETNETWORKPARAMS } = require("dns");
 
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
+const employees = [];
+
+function init(){
+    startHtml();
+    addPerson();
+}
+
+function addPerson(){
+    //need to prompt questions about team member to add
+    inquirer.prompt([{
+        message: "What is member's name?",
+        name: "name"
+    },
+    {
+        type: "list",
+        message: "What is their role?",
+        choices: [
+            "Intern",
+            "Engineer",
+            "Manager"
+        ],
+        name: "role"
+    },
+    {
+        message: "What is team member's email?",
+        name: "email"
+    },
+    {
+        message:"What is team member's ID?",
+        name: "id"
+    }])
+
+}
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
