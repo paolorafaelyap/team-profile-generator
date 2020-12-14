71b039b08a5a7c4800fbb36a2a8fa1d9ee213800
paolorafaelyap <paolorafaelyap@gmail.com>
2020-12-14 15:01:21 -0800

    styling

--- Develop/app.js
+++ Develop/app.js
@@ -120,6 +120,7 @@ function startHtml()
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="../../Assets/style.css">
         <title>Team Profile</title>
     </head>
     <body>
